b35f426bf83e221c5e77b9261ec313cce60caa49
Chris Coralli <ccoralli@gmail.com>
2017-01-31 22:42:31 -0500

    normal mode done.

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,29 @@
-function fibonacci (n)  {
-    var fibby = 0;
-    if (n <= 2)  {
-        fibby += 1;
-    } else  {
-        fibby+= fibonacci(n - 2) + fibonacci(n - 1);
+// STEP 1.
+//function fibonacci (n)  {
+//    var fibby = 0;
+//    if (n <= 2)  {
+//        fibby += 1;
+//    } else  {
+//        fibby += fibonacci(n - 2) + fibonacci(n - 1);
+//    }
+//    return fibby;
+//}
+
+
+var obj = {};
+function fibonacci (n) {
+    if (obj[n]) {
+        console.log(obj);
+        return obj[n];
+    } else {
+        var fibby = 0;
+        if (n <= 2) {
+            fibby += 1;
+        } else {
+            fibby += fibonacci(n - 2) + fibonacci(n - 1);
+        }
+        console.log(obj);
+        obj[n] = fibby;
+        return obj[n];
     }
-    return fibby;
 }
